d9b080a2b76742747647eb041c92ded19d8dd71a
Slaven Mikulic <slaven.mikulic@gmail.com>
2018-01-05 23:17:59 +0100

    Adds event for enter key and refacotring names

--- js/todoEvents.js
+++ js/todoEvents.js
@@ -1,7 +1,8 @@
-const addElement =    $('#add-todo');
-const bodyElement =   $('body');
-const activeElement = $('#active');
-const doneElement =   $('#done');
+const addElement        = $('#add-todo');
+const bodyElement       = $('body');
+const activeElement     = $('#active');
+const doneElement       = $('#done');
+const todoTableElement  = $('.todo-title');
 
 addElement.click(
     () => {
@@ -27,21 +28,44 @@ addElement.keypress((e) => {
 });
 
 addElement.blur(() => {
-    addElement.text('Add Todo');
+    addElement.text('Add todo');
     addElement.removeClass('ta-left');
     addElement.removeClass('p-5px');
     addElement.addClass('ta-center');
 });
 
-// bodyElement.on('dblclick', '.table-td', function () {
-//
-// });
+bodyElement.on('dblclick', (event) => {
+    let target = event.target;
+    target.contentEditable = 'true';
+    target.focus();
+});
 
-bodyElement.on('click', '.checkbox-container', function (e) {
-    let element = e.target.parentNode.parentNode;
+bodyElement.on('focusout', (event) => {
+    let target = event.target;
+    target.removeAttribute('contentEditable');
+    target.selectionStart = 2;
+});
 
-    e.stopPropagation();
-    e.preventDefault();
+todoTableElement.keypress((event) => {
+    if (event.which !== 13) {
+        return;
+    }
+    const target = event.target;
+    const id = target.parentNode.id;
+    const title = target.textContent;
+    const index = getTodoIndex(id);
+    const isFinished = todos[index].isFinished;
+
+    todos[index] = new ToDo(0, title, isFinished);
+    setLocalTodos(todos);
+    target.blur();
+});
+
+bodyElement.on('click', '.checkbox-mark', (event) => {
+    let element = event.target.parentNode.parentNode;
+
+    event.stopPropagation();
+    event.preventDefault();
 
     let id = element.id;
     const index = getTodoIndex(id);
@@ -54,10 +78,21 @@ bodyElement.on('click', '.checkbox-container', function (e) {
     setLocalTodos(todos);
 });
 
-activeElement.click(function () {
+activeElement.click(() => {
     getActiveTodos(todos);
 });
 
-doneElement.click(function () {
+doneElement.click(() => {
     getDoneTodos(todos, true);
+});
+
+bodyElement.on('click', '.todo-delete', () => {
+    const target = event.target;
+
+    const id = target.parentNode.id;
+    const index = getTodoIndex(id);
+    todos.splice(index, 1);
+    setLocalTodos(todos);
+
+    target.parentNode.remove();
 });
